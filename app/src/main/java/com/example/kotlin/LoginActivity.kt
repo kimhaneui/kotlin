f3cbafb2e63c8a766c7b9ab3ae534610d1782077
kimhaneui <kimhaneui2@naver.com>
2020-06-02 16:09:35 +0900

    로그인 버튼 통해서 실제 로그인 시도

--- app/src/main/java/com/example/kotlin/LoginActivity.kt
+++ app/src/main/java/com/example/kotlin/LoginActivity.kt
@@ -2,7 +2,10 @@ package com.example.kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.kotlin.utils.ServerUtil
 import kotlinx.android.synthetic.main.activity_login.*
+import org.json.JSONObject
 
 class LoginActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +19,13 @@ class LoginActivity : BaseActivity() {
         loginBtn.setOnClickListener {
             val email = emailEdt.text.toString()
             val pw = pwEdt.text.toString()
+
+            ServerUtil.postRequestLogin(mContext,email,pw,object : ServerUtil.JsonResponseHandler{
+                override fun onRespons(json: JSONObject) {
+                    Log.d("로그인 응답",json.toString())
+                }
+
+            })
         }
     }
 
