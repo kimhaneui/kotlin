6c929a4e06d453ce3f1aa24a3481f08d4947376d
kimhaneui <kimhaneui2@naver.com>
2020-06-02 15:50:34 +0900

    로그인 화면 구현

--- app/src/main/java/com/example/kotlin/SplashActivity.kt
+++ app/src/main/java/com/example/kotlin/SplashActivity.kt
@@ -8,7 +8,7 @@ class SplashActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_splash)
         setValues()
         setupEvents()
     }
